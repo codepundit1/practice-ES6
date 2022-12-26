a41b0732154250f3a56d190e5a3b3225650751d0
codepundit1 <jahid35-266@diu.edu.bd>
2022-12-26 18:24:25 +0600

    concise method completed

---
+++ conciseMethod/example1.js
@@ -0,0 +1,11 @@
+const Student = {
+    name: 'Jahid Hasan',
+    email: 'jahid@gmail.com',
+
+    age(){
+        return 14;
+    }
+}
+
+console.log(Student.name);
+console.log('My age is ' + Student. age());
