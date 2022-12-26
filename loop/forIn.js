93f1dea1ff928b9366bb34b76d113536676b97c3
codepundit1 <jahid35-266@diu.edu.bd>
2022-12-26 19:07:56 +0600

    template iteral completed and rename folder

---
+++ loop/forIn.js
@@ -0,0 +1,16 @@
+// example 1
+
+let odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+
+for (let odd in odds)
+{
+    console.log(odd);
+}
+
+//example 2
+
+var days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday'];
+
+for (var day in days) {
+      console.log(day);
+}
---
+++ template literal/example.js
@@ -0,0 +1,16 @@
+// example 1
+
+let bangla = 95;
+let english = 80;
+let math = 99;
+
+let avg = (bangla + english + math)
+console.log(
+    `
+    Bangla = ${bangla}
+    English = ${english} 
+    Math = ${math} 
+    Avarage result is: ${avg}
+
+    `
+);
